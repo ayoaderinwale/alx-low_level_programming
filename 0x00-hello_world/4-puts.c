9a01a20a858fac6b822fcc26acf1b149afc62f75
Aderinwale <ayodejiaderinwale@gmail.com>
2022-09-12 19:15:25 -0700

    put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print string with put() function
+ * /
+
+init main(void)
+{
+	char Mystring[50] = "\"Programming is like building a multilingual puzzle"\n;
+
+	   puts(Mystr1);
+	   return 0;
+
+}
