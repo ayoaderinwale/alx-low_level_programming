e6209c8451c9163c0f1f08f3990c29c4ab9cfb4e
ayoaderinwale <110992924+ayoaderinwale@users.noreply.github.com>
2022-09-15 05:40:04 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("a b c d e f g h i  j k  l  m n o p q r s t u v w x y z\n");
+	print_alphabet();
 	return (0);
 }
