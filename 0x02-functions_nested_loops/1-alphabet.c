acc98b880e4b56b582090704ea9998250dee3244
Aderinwale <ayodejiaderinwale@gmail.com>
2022-09-14 22:35:19 -0700

    this siade

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-include "main.h"
+#include "main.h"
 /**
- * C program to print all alphabets from a to z
+ * main -entry point
+ * printf() function
+ * Return: Always 0 (success)
+ * program to print all alphabets from a to z
  */
 
 int main(void)
 {
 	char ch;
+
 	printf("Alphabets from a - z are: \n");
-	for(ch='a'; ch<='z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		printf("%c\n", ch);
 	}
-	return 0;
+	return (0);
 }
