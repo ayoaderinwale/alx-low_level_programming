e6e30b1dfd587c52eff2ccc3d3278e3aedd8ea14
ayoaderinwale <110992924+ayoaderinwale@users.noreply.github.com>
2022-09-15 05:25:49 +0100

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"\n");
+    return (0);
+}
