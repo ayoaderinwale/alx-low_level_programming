7fcaa2d34bb48e6657078e734e9cff64329748e4
Aderinwale <ayodejiaderinwale@gmail.com>
2022-09-14 19:59:36 -0700

    s

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *printf() function
+ *Return: Always 0 (success)
+*/
+
+int main (void)
+
+{
+	printf("\n_putchar\n");
+	
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+nected loop
