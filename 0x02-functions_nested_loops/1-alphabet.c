5e973af47110f8230210468a59776cf4ad819271
Aderinwale <ayodejiaderinwale@gmail.com>
2022-09-14 21:33:22 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    printf("a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"\n");
-    return (0);
+	printf("a b c d e f g h i  j k  l  m n o p q r s t u v w x y z\n");
+	return (0);
 }
