a1b3fef0894f978813f7feb7f2eb65ba12c2c7ce
ayoaderinwale <110992924+ayoaderinwale@users.noreply.github.com>
2022-09-16 05:16:08 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,39 @@
+Task 1.txt
+Who has access
+A
+System properties
+Type
+Text
+Size
+242 bytes
+Storage used
+242 bytes
+Location
+More functions, more nested loops
+Owner
+Antony Bahati
+Modified
+Sep 15, 2022 by Antony Bahati
+Opened
+5:15 AM by me
+Created
+Sep 15, 2022
+No description
+Viewers can download
+#include "main.h"
+
+/**
+*_isdigit - checks whether a  character is a digit or not
+*@c: tested character
+*Return: 1 if it is, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+
+	return (0);
+}
