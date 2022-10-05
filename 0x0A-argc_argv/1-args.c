8530ba06aaa17c1cac222849568e5848f611164c
Aderinwale <ayodejiaderinwale@gmail.com>
2022-10-05 15:34:27 -0700

    the hour

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* main - contains agrc and argv
+ * argv and argv are for compilation
+ *
+ * return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	print("%d\n", argc);
+	return (0);
+}
