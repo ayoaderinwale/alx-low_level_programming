b42900aca4db9cd782d23ecf88e9d88f0221fae6
ayoaderinwale <110992924+ayoaderinwale@users.noreply.github.com>
2022-10-06 00:17:49 +0100

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name number of arguments passed to it
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[]);
+	return (0);
+}
