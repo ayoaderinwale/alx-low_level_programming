c430ddb99c71ebb8cb8f1a2545343c50618652a0
ayoaderinwale <110992924+ayoaderinwale@users.noreply.github.com>
2022-10-08 05:19:03 +0100

    Create main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* void prototypes()
+* int prototypes()
+*/
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
+
+#endif /*MAIN_H*/
