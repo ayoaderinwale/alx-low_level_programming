2cb317cceb88d30f75ab5da2a39aa0ec64bce18f
Aderinwale <ayodejiaderinwale@gmail.com>
2022-10-07 21:00:52 -0700

    how

---
+++ 0x0B-malloc_free/Mex01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * cisfun - function used for concept instroduction
+ * @n1: number of projects
+ * @n2: number of tasks
+ *
+ * Return: nothing
+ */
+void cisfun(unsigned int n1, unsigned int n2)
+{
+	int n;
+	char c;
+	int *ptr;
+	char array[3];
+}
+
+/**
+ * main - concept of introduction for cisfun testing
+ * cisfun - we go again
+ *
+ * Return: nothing.
+ */
+int main(void)
+{
+	cisfun();
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-low level programming in C
+0x07-pointers_arrays_strings
